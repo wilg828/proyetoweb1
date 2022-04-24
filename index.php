a8377e13790d35e455739e7be4b20be86ce5528b
wilg828 <wilg828@gmail.com>
2022-04-24 08:06:29 -0500

    mi primera version de mi app

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+//declaracion de variables
+$var1=23;
+$var2=3;
+$edad=15;
+$res=$var1+$var2;
+//echo "El resultado es: " .$res;
+//echo "La edad es: " .$edad;
+/*if ($edad>=18){
+	echo "Usted es mayor de edad";
+}else {
+	echo "Usted es menor de edad".$edad;
+
+}*/
+for ($i=0; $i < 5; $i++) { 
+	echo "Usted es menor de edad"."<br>";
+}
+?>
